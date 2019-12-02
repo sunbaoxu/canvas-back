20e42c660589fdbc4f1157513a8bcc75107305ac
sunbaoxu <1194063234@qq.com>
2019-12-02 10:44:17 +0800

    aa

---
+++ precache-manifest.ffba4bd758bf77c74ee10f4af9a381ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5d52c19c1e5e69cc944",
+    "url": "js/chunk-vendors.606dd25e.js"
+  },
+  {
+    "revision": "e919cfd7e5ec7fa01894",
+    "url": "js/app.e826dbcf.js"
+  },
+  {
+    "revision": "1f1b73d70e0d27aaaa58",
+    "url": "js/about.e6e41d3e.js"
+  },
+  {
+    "revision": "966170af2a83f8af66dcb384816043e3",
+    "url": "index.html"
+  },
+  {
+    "revision": "c26237659031dc6c47562f4a3e198d54",
+    "url": "img/312241.c2623765.jpg"
+  },
+  {
+    "revision": "e919cfd7e5ec7fa01894",
+    "url": "css/app.c6a1a90d.css"
+  }
+];
